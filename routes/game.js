358f6651bb2ca5f05b3b43befc558349a123bd44
AJ Walter <aj.walter@aol.com>
2019-11-02 14:15:33 -0500

    zzzzzzzzzzzzzzzz

--- routes/game.js
+++ routes/game.js
@@ -2,9 +2,11 @@ const express = require('express');
 const game = express.Router();
 
 game.get('/attack', function(req, res) {
-    let wss = req.app.get("wss");
     let runtime = req.app.get('runtime');
     let player = runtime.get(req.cookies['id']);
+    if (!player) return;
+    runtime.monster.health -= 1;
+    res.type("application/json");
     res.end(JSON.stringify({some: "thing"}));
 });
 
