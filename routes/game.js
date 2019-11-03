92851e6f8f6ea7823646f39660584d55c3eb598a
AJ Walter <aj.walter@aol.com>
2019-11-03 07:01:48 -0600

    Fix damage

--- routes/game.js
+++ routes/game.js
@@ -26,12 +26,14 @@ game.get('/skill/:name', function (req, res) {
 
             damage(runtime, player, req, skill.damage);
 
-            runtime.players.forEach(function (value) {
-                value.health -= skill.damage;
-                if (value.health > value.healthMax) {
-                    value.health = value.healthMax;
-                }
-            });
+            if (name === 'heal') {
+                runtime.players.forEach(function (value) {
+                    value.health -= skill.damage;
+                    if (value.health > value.healthMax) {
+                        value.health = value.healthMax;
+                    }
+                });
+            }
 
             req.app.get('wss').clients.forEach(function each(client) {
                 client.send(JSON.stringify({
