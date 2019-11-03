8495d66735b748877b12d420a513ddcaaf2e1d09
AJ Walter <aj.walter@aol.com>
2019-11-03 04:01:07 -0600

    Skills do the thing

--- routes/game.js
+++ routes/game.js
@@ -6,20 +6,48 @@ game.get('/attack', function(req, res) {
     let player = runtime.get(req.cookies['id']);
     if (!player) return;
 
-    runtime.monster.health -= 1000 * player.level * player.clickMult;
+    damage(runtime, player, 1000 * player.level * player.clickMult);
+
+    player.clicks++;
+    res.type("application/json");
+    res.end(JSON.stringify({status: "ok"}));
+});
+
+game.get('/skill/:name', function (req, res) {
+    let name = req.params.name;
+    let runtime = req.app.get('runtime');
+    let player = runtime.get(req.cookies['id']);
+    if (!player) return;
+
+    for (let i = 0; i < player.skills.length; i++) {
+        let skill = player.skills[i];
+        if (skill.name === name && skill.timer === 0) {
+            skill.timer = skill.cooldown;
+            damage(runtime, player, skill.damage);
+            res.type("application/json");
+            res.end(JSON.stringify({status: "ok"}));
+            return;
+        }
+    }
+
+    res.type("application/json");
+    res.end(JSON.stringify({status: "no"}));
+});
+
+function damage(runtime, player, damage) {
+    runtime.monster.health -= damage;
+
     if (runtime.monster.health <= 0) {
         player.xp += (runtime.monster.healthMax / 10);
+
         while (player.xp >= player.xpreq) {
-            console.log("here");
             player.xp -= player.xpreq;
             player.xpreq *= 1.5;
             player.level++;
         }
+
         runtime.nextMonReal();
     }
-    player.clicks++;
-    res.type("application/json");
-    res.end(JSON.stringify({some: "thing"}));
-});
+}
 
 module.exports = game;
